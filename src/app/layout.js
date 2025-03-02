a110646be030031a07f02a410165293e115111c7
zakir19 <patelzakir784@gmail.com>
2025-03-02 10:00:29 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Chat app",
-  description: "organize your mettings easily with chatter yes i am here",
+  description: "organize your mettings easily with chatter",
 };
 
 import {
