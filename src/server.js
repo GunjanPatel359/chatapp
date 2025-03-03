f4167075aad50cfa8805a65cfb39a791f189b061
gunjan patel <g.a.patel359@gmail.com>
2025-03-03 20:27:13 +0530

    work

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@
 
 // export const socket = io();
 
-// export const webSocketServer="https://socket-for-chatapp.onrender.com"
-export const webSocketServer="http://localhost:4000"
+export const webSocketServer="https://socket-for-chatapp.onrender.com"
+// export const webSocketServer="http://localhost:4000"
